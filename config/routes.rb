989774d9ae75c23f613e45b926297e91da05e7f2
Hieu Nguyen <nguyen.minh.hieu@sun-asterisk.com>
2020-07-29 23:16:29 -0700

    chap4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope "(:locale)", locale: /vi|en/ do
     root "static_pages#home"
     
